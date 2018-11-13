301908a97db4f05892cb9db1c9b151287ec9df4c
Elina Gazizova <lady.gazizova2011@yandex.ru>
2018-11-13 17:02:56 +0300

    no message

---
+++ SyntaxPart.py
@@ -0,0 +1,5 @@
+class SyntaxPart:
+    def __init__(self, text, pretext=0, info=0):
+        self.text = text
+        self.pretext = pretext
+        self.info = info
--- TextParser.py
+++ TextParser.py
@@ -1,30 +1,36 @@
 from textblob import TextBlob
 import Pretexts
+from SyntaxPart import SyntaxPart
+import numpy as np
 
 
 class TextParser:
 
-    def break_to_parts(self, sentence):
-        part = sentence
-        for word in part.split():
-            for pretext in Pretexts.place:
-                if word == pretext:
-                    part = part.split(pretext)
-                    print(1)
+    def break_to_parts(self, sentence_original):
+        sentence_massive = {SyntaxPart(sentence_original)}
+        for syntaxP in sentence_massive:
             for pretext in Pretexts.cause_target:
-                if word == pretext:
-                    part = part.split(pretext)
-                    print(2)
-                    print(part)
-            for pretext in Pretexts.direction:
-                if word == pretext:
-                    part = part.split(pretext)
-                    print(3)
-            for pretext in Pretexts.time:
-                if word == pretext:
-                    part = part.split(pretext)
-                    print(4)
-        print(part)
+                if pretext in syntaxP.text:
+                    self.split_part(syntaxP, pretext, "cause_target")
+            #        part = part.split(pretext)
+            #        print(1)
+            # for pretext in Pretexts.cause_target:
+            #     if word == pretext:
+            #         part = part.split(pretext)
+            #         print(2)
+            #         print(part)
+            # for pretext in Pretexts.direction:
+            #     if word == pretext:
+            #         part = part.split(pretext)
+            #         print(3)
+            # for pretext in Pretexts.time:
+            #     if word == pretext:
+            #         part = part.split(pretext)
+            #         print(4)
+
+    def split_part(self, part, pretext, type_pretext):
+        new_part = SyntaxPart(part.text[part.text.index(pretext):], pretext, type_pretext)
+        print(new_part.text)
 
     def __init__(self, text):
         info = TextBlob(text)
