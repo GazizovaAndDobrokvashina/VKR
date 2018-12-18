e2a20c4bf82c1ce7e2b3549f5de8beb6df27e0ff
Dobrokvashina Alexandra <aleksandra_dobro@list.ru>
2018-12-18 14:16:09 +0300

    parser change

--- VKR/Russian/Parser.py
+++ VKR/Russian/Parser.py
@@ -58,7 +58,7 @@ class Parser:
                     for n in noise:
                         line = line.replace(n, '')
                         base_name = base_name.replace(n, '')
-                    names = self.parse_names(line.replace('-',' '))
+                    names = self.parse_words(line.replace('-',' '))
                     self.actors[base_name] = names
                 if next_chapter:
                     self.chapter = Chapter('Сюжет', line, self.actors, self.artifacts)
@@ -70,7 +70,7 @@ class Parser:
                     for n in noise:
                         line = line.replace(n, '')
                         base_name = base_name.replace(n, '')
-                    names = self.parse_names(line.replace('-', ' '))
+                    names = self.parse_words(line.replace('-', ' '))
                     self.artifacts[base_name] = names
 
             for key in self.actors:
@@ -112,7 +112,111 @@ class Parser:
 
         return nams
 
-    
+    def parse_words(self, words):
+        words = words.split()
+        noise = [',', ':', '\n']
+        clear_words = []
+        for word in words:
+            for n in noise:
+                word.replace(n, '')
+            clear_words.append(word.lower())
+
+        types = [morph.parse(word)[0] for word in clear_words]
+
+        exeptions = {'кость': ['NOUN', 'кость'], 'рыжий': ['ADJF', 'рыжий'], 'молодой': ['ADJF', 'молодой']}
+
+        for index in range(len(clear_words)):
+            if clear_words[index] in exeptions.keys():
+                for typ in morph.parse(clear_words[index]):
+                    if typ.tag.POS == exeptions[clear_words[index]][0] \
+                            and typ.normal_form == exeptions[clear_words[index]][1]:
+                        types[index] = typ
+                        break
+
+        search_for = [[['ADJF', 0], ['NOUN', 0]],
+                      [['NOUN', 0], ['NOUN', 1]],
+                      [['NOUN', 0], ['ADJF', 1], ['NOUN', 1]],
+                      [['ADJF', 0], ['ADJF', 0], ['NOUN', 0]],
+                      [['NOUN', 0]]]
+
+        result = []
+        for seq in search_for:
+            indexes = self.search_sequence(types, seq)
+            nums = self.multi_adj(seq)
+            noun_place = seq.index(['NOUN', 0])
+            for i in indexes:
+                name = clear_words[i:i+len(seq)]
+                res = ''
+                for i in range(len(seq)):
+                    res+= name[i] + ' '
+                result.append(res)
+                if len(nums) > 1:
+                    noun =  name[noun_place]
+                    for i in nums:
+                        add = name[i] + ' ' + noun
+                        result.append(add)
+
+        return result
+
+
+
+    def multi_adj(self, sequence):
+        nums = []
+        for i in range(len(sequence)):
+            if sequence[i] == 'ADJF':
+                nums.append(i)
+
+        return nums
+
+    def search_sequence(self, sentence, sequence):
+        j = 0
+        founds = []
+        curr = []
+        indexes = []
+        cur = 0
+        for i in range(len(sentence)):
+            if sentence[i].tag.POS == sequence[j][0]:
+                if j==0:
+                    cur = i
+                curr.append(sentence[i])
+                j += 1
+                if j == len(sequence):
+                    founds.append(curr)
+                    curr = []
+                    j = 0
+                    indexes.append(cur)
+            else:
+                if j != 0:
+                    j = 0
+                    curr = []
+
+        for_delete = []
+        for i in range(len(founds)):
+            words = {0:[], 1:[]}
+            for index in range(len(founds[i])):
+                words[sequence[index][1]].append(founds[i][index])
+
+            for word in words[0]:
+                if word.tag.case!= 'nomn' and i not in for_delete:
+                    for_delete.append(i)
+
+            if len(words[1]) > 0:
+                case = words[1][0].tag.case
+                for word in words[1]:
+                    if word.tag.case != case and i not in for_delete:
+                        for_delete.append(i)
+
+        n=0
+        for i in for_delete:
+            founds.remove(founds[i-n])
+            indexes.remove(indexes[i-n])
+            n+=1
+        print(sequence)
+        print(founds)
+        print()
+
+        return indexes
+
 
 
 
